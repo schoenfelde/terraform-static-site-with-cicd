297b5b58b009091ffd1a68a51757fab5f81dc419
Elliot Schoenfeld <elliot@ollieware.com>
2020-04-20 10:44:02 +0530

    remove www

--- modules/dns/main.tf
+++ modules/dns/main.tf
@@ -9,14 +9,3 @@ resource "aws_route53_record" "base" {
     evaluate_target_health = true
   }
 }
-
-resource "aws_route53_record" "www" {
-  zone_id = var.r53_zone_id
-  name    = "www.${var.website_name}.com"
-  type = "A"
-  alias {
-    name                   = var.domain_name
-    zone_id                = var.zone_id
-    evaluate_target_health = true
-  }
-}
